6c7abbac46589c2742a4a6aca7a1e5a359f1ef3f
evenflowvisualart <bubnic.marina@gmail.com>
2018-01-26 15:26:53 +0100

    add
    additional code

--- js/functionality.js
+++ js/functionality.js
@@ -1,4 +1,24 @@
 
+$(document).ready(function(){
+    
+// Add minus icon for collapse element which is open by default
+    
+    $('.collapse.in').each(function(){
+        $(this).siblings('.panel-heading').find('.fa').addClass('fa-caret-down').removeClass('fa-caret-right');
+    });
+    
+// Toggle plus minus icon on show hide of collapse element
+    
+    $('.collapse').on('show.bs.collapse', function(){
+      $(this).parent().find('.fa').removeClass('fa-caret-right').addClass('fa-caret-down');
+    }).on('hide.bs.collapse', function(){
+        $(this).parent().find(".fa").removeClass('fa-caret-down').addClass('fa-caret-right');
+    });
+
+
+});
+
+
 //navbar appear on scroll
 
 //content appear on scroll
